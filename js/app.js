c91931025eabbe986fc7b1c5e40be4add00b89d2
Yousef <yousef@live.ca>
2016-07-01 19:17:51 +0100

    required methods added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var Enemy = function(x,y) {
     // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // a helper provided to load images
     this.sprite = 'images/enemy-bug.png';
+
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 250) + 190);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +15,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed *dt;
+
+    // If statement so the enemies reset when
+    // they reach end of the canvas
+    if (this.x > 500)
+        this.x = -90;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,21 +32,75 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+
+    this.sprite = 'images/char-pink-girl.png';
+    this.reset();
+};
+
+// A reset method for the player is added
+Player.prototype.reset = function() {
+    this.x = 202;
+    this.y = 405;
+};
+
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// A handleInput method is added using if-statements
+// althought a switch statement is suitable as well
+Player.prototype.handleInput = function(key){
+    if (key === "down")
+        this.y += 23;
+    else if (key === "up")
+        this.y -= 23;
+    else if (key === "left")
+        this.x -= 23;
+    else if (key === "right")
+        this.x += 23;
+
+    this.boundries();
+};
+
+// Here we set the boundries for the Player
+// and add an alert if the game is won
+Player.prototype.boundries = function () {
+    if (this.x < -5)
+    this.x = -5;
+    else if (this.x > 409)
+    this.x = 409;
+    else if (this.y > 428)
+        this.y = 428;
+    else if (this.y <= -9){
+        alert("Well done!! You've won!");
+        this.reset();
+    }
+};
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var allEnemies = [
+new Enemy(-5,60),
+new Enemy(-5,143),
+new Enemy(-5,225),
+new Enemy(-5,307)
+];
+var player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener("keyup", function(e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
--- js/engine.js
+++ js/engine.js
@@ -112,7 +112,7 @@ var Engine = (function(global) {
                 'images/stone-block.png',   // Row 1 of 3 of stone
                 'images/stone-block.png',   // Row 2 of 3 of stone
                 'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/grass-block.png',   // Row 1 of 2 of grass
+                'images/stone-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
             numRows = 6,
@@ -171,7 +171,7 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-pink-girl.png'
     ]);
     Resources.onReady(init);
 
