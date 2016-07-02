2ca38d10baba014750a595e129602e45f84e6f4a
Yousef <yousef@live.ca>
2016-07-02 20:13:44 +0100

    Added collision method to Enemy

--- js/app.js
+++ js/app.js
@@ -3,10 +3,9 @@ var Enemy = function(x,y) {
     // The image/sprite for our enemies, this uses
     // a helper provided to load images
     this.sprite = 'images/enemy-bug.png';
-
     this.x = x;
     this.y = y;
-    this.speed = Math.floor((Math.random() * 250) + 190);
+    this.speed = Math.floor((Math.random() * 150) + 100);
 };
 
 // Update the enemy's position, required method for game
@@ -15,12 +14,24 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x += this.speed *dt;
-
+    this.x += this.speed * dt;
     // If statement so the enemies reset when
     // they reach end of the canvas
     if (this.x > 500)
         this.x = -90;
+    this.collision();
+};
+
+// a collision method is added where it checks
+// if the player and the enemy collide then
+// resets the player if they do
+Enemy.prototype.collision = function() {
+    if (player.x +50 > this.x &&
+        player.x     < this.x + 65 &&
+        player.y +60 > this.y &&
+        player.y     < this.y + 70 ) {
+        player.reset();
+}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -28,13 +39,10 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+// The Player Class includes a reset(), update(),
+// render() handleInput(), and boundries() method.
 var Player = function() {
-
-    this.sprite = 'images/char-pink-girl.png';
+    this.sprite = 'images/bernie.png';
     this.reset();
 };
 
@@ -55,24 +63,23 @@ Player.prototype.render = function() {
 // althought a switch statement is suitable as well
 Player.prototype.handleInput = function(key){
     if (key === "down")
-        this.y += 23;
+        this.y += 25;
     else if (key === "up")
-        this.y -= 23;
+        this.y -= 25;
     else if (key === "left")
-        this.x -= 23;
+        this.x -= 25;
     else if (key === "right")
-        this.x += 23;
-
+        this.x += 25;
     this.boundries();
 };
 
 // Here we set the boundries for the Player
 // and add an alert if the game is won
 Player.prototype.boundries = function () {
-    if (this.x < -5)
-    this.x = -5;
-    else if (this.x > 409)
-    this.x = 409;
+    if (this.x < -23)
+        this.x = -23;
+    else if (this.x > 427)
+        this.x = 427;
     else if (this.y > 428)
         this.y = 428;
     else if (this.y <= -9){
@@ -81,15 +88,14 @@ Player.prototype.boundries = function () {
     }
 };
 
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [
-new Enemy(-5,60),
-new Enemy(-5,143),
-new Enemy(-5,225),
-new Enemy(-5,307)
+new Enemy(0,60),
+new Enemy(500,143),
+new Enemy(400,225),
+new Enemy(100,307)
 ];
 var player = new Player();
 
